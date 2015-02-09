a5507348894a2ffc8a0908c60e523824b55e244f
Nathan Samano <nsv9982@saintjoe.edu>
2015-02-09 14:09:51 -0500

    Added comments to identify query to problem

--- 4.10.sql
+++ 4.10.sql
@@ -1,5 +1,8 @@
+-- a
 select distinct e.fname, e.lname from employee as e, works_on as w where e.dno=5 and w.pno=1 and e.ssn=w.essn and w.hours > 10;
 
+-- b
 select distinct e.fname, e.lname from employee as e, dependent as d where d.dependent_name=e.fname and d.essn=e.ssn;
 
+-- c
 select distinct fname, lname from employee where super_ssn='333445555';
